3aa57b861a2834483a83cc7b32e35ad87c1fc1da
Abdul Rahim <abdul7716@gmail.com>
2020-10-07 17:28:55 +0530

    added some UI files for like save screen

---
+++ ImageAppUnsplash/ImageAppUnsplash/View/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageAppUnsplash
+//
+//  Created by Abdul chaudhary on 07/10/20.
+//
+
+import Foundation
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
